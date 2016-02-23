acbc6069ef688d8d4164251a92345b0dd21259b8
Michael Hammer <mph009@bucknell.edu>
2016-02-23 14:23:16 -0500

    small gen fix

--- python/genPage.py
+++ python/genPage.py
@@ -2,7 +2,7 @@
 import re
 import sys
 
-htmlCap = "<!DOCTYPE html><html><head><title>Web Crawler</title><link rel='stylesheet' type='text/css' href='HTML_Styling2.css'></head><body><div class='header'>CSCI 363 Search Engine by Michael Hammer, Greg Shrock, & Dunni Something</div><div class='title'>WEB CRAWLER</div><div class='submitRow'><form method='POST' action='/form'><input class='textBox' type='text' name='input'><input class='submitBtn' type='submit' name='Submit' value='submit'></form></div><div class='links'>"
+htmlCap = "<!DOCTYPE html><html><head><title>Web Crawler</title><link rel='stylesheet' type='text/css' href='HTML_Styling.css'></head><body><div class='header'>CSCI 363 Search Engine by Michael Hammer, Greg Shrock, & Dunni Something</div><div class='title'>WEB CRAWLER</div><div class='submitRow'><form method='POST' action='/form'><input class='textBox' type='text' name='input'><input class='submitBtn' type='submit' name='Submit' value='submit'></form></div><div class='links'>"
 htmlTail = "</div></body></html>"
 
 
