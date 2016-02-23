ebfa95e965cb2c352dc301d1b8e76a09636d2fb2
Michael Hammer <mph009@bucknell.edu>
2016-02-23 13:04:06 -0500

    added file to generate new page

---
+++ web/genPage.py
@@ -0,0 +1,16 @@
+# Michael Hammer & Greg Shrock & Dunni
+import re
+import sys
+
+htmlCap = "<!DOCTYPE html><html><head><title>Web Crawler</title><link rel='stylesheet' type='text/css' href='HTML_Styling2.css'></head><body><div class='header'>CSCI 363 Search Engine by Michael Hammer, Greg Shrock, & Dunni Something</div><div class='title'>WEB CRAWLER</div><div class='submitRow'><form method='POST' action='/form'><input class='textBox' type='text' name='input'><input class='submitBtn' type='submit' name='Submit' value='submit'></form></div><div class='links'>"
+htmlTail = "</div></body></html>"
+
+
+links = ""
+l = ""
+while(l != 'terminate\n'):
+  links = links + l
+  l = sys.stdin.readline()
+
+f = open("linkPage.html",'w')
+f.write(htmlCap+links+htmlTail)
