7dc7bdac713d5ba9cd32cbb88c1798704b13c3e3
Dunni Adenuga <ia005@bucknell.edu>
2016-03-03 14:06:27 -0500

    added comments

--- include/queryFinder.h
+++ include/queryFinder.h
@@ -1,3 +1,4 @@
+/*Dunni , Michael and Greg*/
 #include "stdio.h"
  #include <sys/types.h>
  #include <sys/stat.h>
@@ -7,12 +8,44 @@
 #include<string.h>
 #include<stdlib.h>
 
+/*listOfWords - words in a webpage
+  stopWordList - stop words */
+/*
+Inserts word into listOfWords one at a time 
+ */
 void insertWord(char *word);
+/*
+Assumes words are in a file and inserts them into listOfWords at once
+ */
 void putWordsinList(void);
+/*
+Sees if word is in listOfWords
+ */
 bool searchQuery(char* target);
+/*
+Create a stop-words list 
+ */
 void createStopList(void);
+/*
+see if a word is in the stopWordList before adding to list Of Words
+ */
 bool searchStopList(char* word);
+/*
+Sort
+*/
 void sortListOfWords(void);
+/*
+  creates initial empty lists
+*/
 void initFinder();
+
+/*
+Manages whole Program
+Create, sort and search for query.
+ */
 bool dotheWork(char * query);
+
+/*
+Print out list
+ */
 void traverse_forward(struct dlist *l);
